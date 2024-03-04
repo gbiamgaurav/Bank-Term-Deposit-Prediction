2c4ddbda21228516755832c7e61497edc3a8cf07
gbiamgaurav <gaurav.bhattacharya10@gmail.com>
2024-03-04 16:11:38 +0530

    structure added

---
+++ template.py
@@ -0,0 +1,53 @@
+
+import os 
+from pathlib import Path 
+import logging
+
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = "mlproject"
+
+list_of_files = [
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/entity/config_entity.py",
+    f"src/{project_name}/entity/artifact_entity.py",
+    f"src/{project_name}/constants/__init__.py",
+    f"src/{project_name}/test/__init__.py",
+    f"src/{project_name}/test/unit_test.py",
+    f"src/{project_name}/test/integration_test.py",
+    "tox.ini",
+    "pyproject.toml",
+    "config/config.yaml",
+    "params.yaml",
+    "schema.yaml",
+    "main.py",
+    "app.py",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "init_setup.sh"
+
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+
+    filedir, filename = os.path.split(filepath)
+
+    if filedir != "":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory: {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+    else:
+        logging.info(f"{filename} is already exists")
